e4274fbbd985cd5fff08bf546adbd4cc830351e9
LucArte <lucatobonm@gmail.com>
2022-06-02 23:13:57 +0200

    Allow full access through CORS

--- index.js
+++ index.js
@@ -13,29 +13,9 @@ app.use(express.static('public'));
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors()); // This uses CORS and allows all domains to have access to the app
 
 const { check, validationResult } = require('express-validator');
-
-let allowedOrigins = [
-    'http://localhost:8080',
-    'https://your-favorite-books.herokuapp.com/',
-    'https://localhost:1234'
-];
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                // If a specific origin isn’t found on the list of allowed origins
-                let message =
-                    "The CORS policy for this application doesn’t allow access from origin " +
-                    origin;
-                return callback(new Error(message), false);
-            }
-            return callback(null, true);
-        },
-    })
-);
 
 // Use passport from external files
 let auth = require('./auth')(app);
